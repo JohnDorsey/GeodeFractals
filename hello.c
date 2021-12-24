efc51f1fbbed9b05eeeb345c645a261707c8cfcb
john <laptopjohn10@gmail.com>
2021-12-24 13:56:46 -0600

    first commit

---
+++ hello.c
@@ -0,0 +1,324 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <complex.h>
+#include <math.h>
+
+// on all builds before 16 nov 2021, color is decided based on drawR and drawI, and the only effect that can be applied after color is decided is iterslide.
+
+//static const char LUMABET[10] = {'.', '`', ',', '-', '~', '=', '%', '$', '&', '#'};
+//static const int LUMABET_LEN=10;
+
+
+// ----- constant settings --------------------
+#define CHANNEL_COUNT 3
+
+
+
+// ----- canvas settings -----------------------
+#define WIDTH 2048
+#define HEIGHT 2048
+static const int ITERLIMIT=512;
+static const int PRINT_INTERVAL=4096;
+static const int SUPERSAMPLING=8;
+
+// ----- generation settings ---------------------------
+//mandelbrot:
+//#define FRACTAL_FORMULA tmpZr = zr;	tmpZi = zi; zr = tmpZr*tmpZr - tmpZi*tmpZi + cr; zi = 2.0*tmpZr*tmpZi + ci;
+//julia:
+#define FRACTAL_FORMULA tmpZr = zr;	tmpZi = zi; zr = tmpZr*tmpZr - tmpZi*tmpZi - 0.755; zi = 2.0*tmpZr*tmpZi + 0.15;
+
+#define Z_STARTS_AT_C 1 //absolutely necessary for julia set buddhabrot generation.
+
+#define JOINT_BUDDHABROT 1
+static const bool INVERT_BUDDHABROT=false;
+
+// ----- drawing settings -------------------------
+#define DO_MEAN_OF_ZSEQ 0
+static const int ITERSLIDE_REAL=0;
+static const int ITERSLIDE_IMAG=0;
+//static const int POINTS_PER_LINE_SEGMENT=4096;
+
+// ----- output settings ------------------------
+#define LOG_COLORS 0
+#define WRAP_COLORS 0
+static const float COLOR_POWER=0.5;
+static const float COLOR_SCALE=1.0;
+
+
+
+
+// ----- necessary settings
+static int globalScreenArr[HEIGHT][WIDTH][CHANNEL_COUNT];
+static const int ZERO=0;
+
+
+
+
+int min(int a, int b) {
+	return ((a<b)? a : b);
+}
+int max(int a, int b) {
+	return ((a<b)? b : a);
+}
+
+void print_screen(int (*screenArr)[HEIGHT][WIDTH][CHANNEL_COUNT]) {
+	float floatVal;
+	int intVal;
+	for ( int y = 0; y < HEIGHT; y++ ) {
+		printf("[");
+		for ( int x = 0; x < WIDTH; x++) {
+			printf("(");
+			for ( int c = 0; c < CHANNEL_COUNT; c++ ) {
+				floatVal = (float) ((*screenArr)[y][x][c]);
+				#if LOG_COLORS
+					floatVal = logf(floatVal + 1.0);
+				#endif
+				floatVal = (pow(floatVal, COLOR_POWER) * COLOR_SCALE);
+				intVal = (int) floatVal;
+				#if WRAP_COLORS
+					intVal = intVal % 256;
+				#endif
+				intVal = max(min(intVal, 255), 0);
+				if (c < (CHANNEL_COUNT - 1)) {
+					printf("%d,", intVal);
+				} else {
+					printf("%d", intVal);
+				}
+			}
+			printf("),");
+		}
+		printf("]\n");
+	}
+	/* printf("[");
+	for ( int hackx = 0; hackx < WIDTH; hackx++ ) {
+		printf("254,");
+	}
+	printf("]\n");
+	*/
+	return;
+}
+
+int to_screen_coord(float v, int screenMeasure) {
+	//v = (v+2.0)/4.0;
+	// assert(v <= 1);
+	// assert(v >= 0);
+	//int result = ((int) (((v+2.0)/4.0 ) * ((float) screenMeasure))); // <-- almost works.
+	// assert(result >= 0);
+	// assert(result < screenMeasure);
+	//int result = ((int) (v * ((float) screenMeasure))) % screenMeasure;
+	//return 0; //---------------------------------------------------------------------------------------------------------------------------
+	
+	//assert(HEIGHT > 10);
+	//assert(WIDTH > 10);
+	/*
+	float vShrunk;
+	vShrunk = (v+2.0) / 4.0;
+	float vLarge;
+	assert(screenMeasure > 10);
+	vLarge = vShrunk * ((float) screenMeasure);
+	int result;
+	result = (int) vLarge;
+	
+	return result;
+	*/
+	return (int) ((v+2.0) * 0.25 * ((float) screenMeasure)); //0.25 is just the inverse of 4 here.
+}
+
+float from_screen_coord(int x, int screenMeasure) {
+	//return 0.5;
+	/*
+	float denom;
+	denom = (float) screenMeasure;
+	assert(denom > 1.5);
+	float vSmall;
+	vSmall = ((float) x)/denom;
+	return ((float) ((vSmall*4.0)-2.0));
+	*/
+	return (( ((float) x) / ((float) screenMeasure) * 4.0) - 2.0);
+}
+
+
+
+
+
+
+int calc_mandelbrot_point(float cr, float ci) {
+	#if Z_STARTS_AT_C
+		float zr=cr; float zi=ci;
+	#else
+		float zr=0.0; float zi=0.0;
+	#endif
+	float tmpZr; float tmpZi;
+	//float cr=real; float ci=imag;
+	for ( int i = 0; i < ITERLIMIT; i++ ) {
+		//tmpZr = zr*zr - zi*zi; tmpZi = 2.0*zr*zi; zr = tmpZr + cr; zi = tmpZi + ci;
+		FRACTAL_FORMULA
+		if ( (zr*zr + zi*zi) > 16 ) {
+			return i;
+		}
+	}
+	return 0;
+}
+
+
+
+bool jointbrot_point_should_be_skipped(float cr, float ci) {
+	/*if ( ! JOINT_BUDDHABROT ) {
+		if ( (calc_mandelbrot_point(cr, ci) <= 0) != INVERT_BUDDHABROT ) {
+			return true;
+		}
+	}
+	return false;*/
+	#if JOINT_BUDDHABROT
+		return false;
+	#else
+		if ( (calc_mandelbrot_point(cr, ci) <= 0) != INVERT_BUDDHABROT ) {
+			return true;
+		}
+		return false;
+	#endif
+}
+
+void do_jointbrot_point(float cr, float ci, int (*screenArr)[HEIGHT][WIDTH][CHANNEL_COUNT]) {
+
+	if (jointbrot_point_should_be_skipped(cr, ci)) { return; }
+
+	//printf("\ncr%f ci%f w%d h%d:",cr,ci,WIDTH,HEIGHT);
+	//float cAbsSquared=cr*cr + ci*ci;
+	//float zAbsSquared;
+	#if Z_STARTS_AT_C
+		float zr=cr; float zi=ci;
+	#else
+		float zr=0.0; float zi=0.0;
+	#endif
+	float tmpZr; float tmpZi;
+	int drawX; int drawY;
+	
+	float zrSum = 0.0; float ziSum = 0.0;
+	float drawR = 0.0; float drawI = 0.0;
+	
+		/* tmpZr = zr*zr - zi*zi; tmpZi = 2.0*zr*zi;	zr = tmpZr + cr; zi = tmpZi + ci; */
+	int iterationIndex;
+	//int iii;
+	//float initialDrawR = 0.0; float finalDrawR = 0.0; float initialDrawI = 0.0; float finalDrawI = 0.0;
+	
+	for ( iterationIndex = 0; iterationIndex < ITERLIMIT; iterationIndex++ ) {
+		//(*screenArr)[iterationIndex % HEIGHT][iterationIndex % WIDTH][0] += 1;
+		//printf("iter%d starts zr=%f zi=%f.", iterationIndex, zr, zi);
+		FRACTAL_FORMULA
+		//printf("now zr=%f zi=%f.",zr,zi);
+		if ( (zr*zr + zi*zi) > 16.0 ) {
+			return;
+		}
+		//(*screenArr)[iterationIndex % HEIGHT][iterationIndex % WIDTH][1] += 1;
+		
+		//initialDrawR = drawR; initialDrawI = drawI;
+		#if DO_MEAN_OF_ZSEQ
+			zrSum += zr; ziSum += zi;
+			drawR = zrSum/(ii+1.0); drawI = ziSum/(ii+1.0);
+		#else
+			drawR = zr; drawI = zi;
+		#endif
+		//finalDrawR = drawR; finalDrawI = drawI;
+		//assert(drawR==zr);
+
+		drawX = to_screen_coord(drawR, WIDTH) + (iterationIndex * ITERSLIDE_REAL);
+		drawY = to_screen_coord(drawI, HEIGHT) + (iterationIndex * ITERSLIDE_IMAG);
+		
+		if ( (drawX>=0) && (drawX<WIDTH) && (drawY>=0) && (drawY<HEIGHT) ) {
+		    //(drawX>=0) && (drawX<WIDTH) && (drawY>=0) && (drawY<HEIGHT)
+			//( ! (drawX>=WIDTH || drawX < 0 || drawY>=HEIGHT || drawY < 0) )
+			(*screenArr)[drawY][drawX][0] += 1;
+			if ( drawR > cr ) { (*screenArr)[drawY][drawX][1] += 1; }
+			if ( drawI > ci ) {	(*screenArr)[drawY][drawX][2] += 1;	}
+			//printf("draw success:");
+		} else {
+			//printf("draw failure:");
+		}
+		
+		//(*screenArr)[iterationIndex % HEIGHT][iterationIndex % WIDTH][2] += 1;
+		//printf("%f %f %d %d %d %d\n",drawR,drawI,drawX,drawY,WIDTH,HEIGHT);
+		
+	}
+	return;
+}
+
+
+
+		/* tmpZr = zr*zr - zi*zi; tmpZi = 2.0*zr*zi;	zr = tmpZr + cr; zi = tmpZi + ci; */
+/*
+if ( ii == 0 ) { continue; }
+		for (iii = 0; iii < POINTS_PER_LINE_SEGMENT; iii++) {
+			drawR = initialDrawR + ((finalDrawR - initialDrawR)/((float) POINTS_PER_LINE_SEGMENT))*(((float) iii)+1.0);
+			drawI = initialDrawI + ((finalDrawI - initialDrawI)/((float) POINTS_PER_LINE_SEGMENT))*(((float) iii)+1.0);
+			
+			...drawX drawY
+
+			..mark screenArr.
+		}
+*/
+
+
+void do_mandelbrot() {
+	int val;
+	//printf("[");
+	for ( int y = 0; y < HEIGHT; y++ ) {
+		printf("[");
+		for ( int x = 0; x < WIDTH; x++) {
+			val = calc_mandelbrot_point(from_screen_coord(x, WIDTH), from_screen_coord(y, HEIGHT));
+                        // val = (val)*(LUMABET_LEN-1)/ITERLIMIT;
+			val = val * 256 / ITERLIMIT;
+			//printf("%c", LUMABET[val]);
+			assert(val<256);
+			printf("(%d,0,0),", val);
+		}
+		printf("]\n");
+	}
+	//printf("]");
+	return;
+}
+
+void build_buddhabrot(int supersampling, int (*screenArr)[HEIGHT][WIDTH][CHANNEL_COUNT]) {
+	int superHeight = HEIGHT*supersampling;
+	int superWidth = WIDTH*supersampling;
+	printf("# buddhabrot build started.\n");
+	for ( int y = 0; y < superHeight; y++ ) {
+		if ( ((y % PRINT_INTERVAL) == 0) && (y != 0) ) {
+			printf("# %d of %d rows processed.\n", y, superHeight);
+			print_screen(screenArr);
+		}
+		for ( int x = 0; x < superWidth; x++) {
+			do_jointbrot_point(from_screen_coord(x, superWidth), from_screen_coord(y, superHeight), screenArr);
+			//printf("(%d,%d) ", x, y);
+		}
+	}
+}
+
+
+
+int main(int argc, char **argv) {
+	printf("# hello.c running.\n");
+	//printf("\n");
+	assert(HEIGHT > 10);
+	assert(WIDTH > 10);
+	assert(to_screen_coord(0.01, 256) > 124);
+	assert(to_screen_coord(0.01, 256) < 132);
+	
+	assert(to_screen_coord(from_screen_coord(177, 256), 256) - 177 < 3);
+	//printf("# %f", from_screen_coord(24, 256));
+	assert(to_screen_coord(from_screen_coord(24, 256), 256) - 24 < 8);
+
+	//	static int screenArr[HEIGHT][WIDTH];
+	memset(globalScreenArr, ZERO, sizeof globalScreenArr);
+	printf("# hello.c ready.\n");
+	//return 0;
+	build_buddhabrot(SUPERSAMPLING, &globalScreenArr);
+	//do_mandelbrot(SUPERSAMPLING, &globalScreenArr);
+	// return 0;
+	print_screen(&globalScreenArr);
+	printf("STOP");
+	return 0;
+}
---
+++ photo.py
@@ -0,0 +1,454 @@
+#!/usr/bin/python3
+
+"""
+todo:
+  -core:
+    -simplify. Make only streaming.
+    -replace builtin eval with a custom evaluator that has good error handling and no size limit.
+    -accept any arguments through stdin as well as the usual way.
+    -pypy support.
+  -etc:
+    -support 16bit png output.
+    -accept pam files as input and as an output format.
+    -allow tiny pygame previews of a file under construction.
+    -user-defined kernels:
+      -transform data in transit to another application.
+      -output in multiple resolutions.
+"""
+
+
+import sys
+# print(sys.argv[0])
+import math
+import itertools
+import time
+import collections
+
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+try:
+    import pygame
+except ImportError:
+    pygame = None
+    
+try:
+    import png
+except ImportError:
+    png = None
+
+NOT_IMPLEMENTED_ERRLVL = 255
+
+
+
+def gen_take_only(input_gen, count):
+    for i in range(count):
+        try:
+            currentItem = next(input_gen)
+        except StopIteration:
+            return
+        yield currentItem
+    return
+    
+def gen_make_inexhaustible(input_gen):
+    for item in input_gen:
+        yield item
+    while True:
+        yield item
+
+def product(input_seq):
+    result = 1
+    for item in input_seq:
+        result *= item
+    return result
+
+
+def shape(data_to_test):
+    result = []
+    while hasattr(data_to_test, "__len__"):
+        result.append(len(data_to_test))
+        if result[-1] == 0:
+            break
+        data_to_test = data_to_test[0]
+    return result
+    
+def labled_shape(data_to_test, access_order):
+    result = dict()
+    while hasattr(data_to_test, "__len__"):
+        assert access_order[0] not in result
+        result[access_order[0]] = len(data_to_test)
+        data_to_test, access_order = data_to_test[0], access_order[1:]
+    return result
+
+
+def higher_range(input_list, iteration_order=None):
+    if iteration_order is not None:
+        newInputList = [None for i in range(len(input_list))]
+        for i, newLocation in enumerate(iteration_order):
+            newInputList[newLocation] = input_list[i]
+        for item in higher_range(newInputList, iteration_order=None):
+            yield tuple(item[iteration_order[i]] for i in range(len(iteration_order)))
+    settings_list = [None for i in range(len(input_list))]
+    for i, item in enumerate(input_list):
+        newItem = None
+        if type(item) == int:
+            newItem = (0,item,1)
+        elif type(item) == tuple:
+            if len(item) == 2:
+                newItem = item + (1,)
+            elif len(item) == 3:
+                newItem = item
+            else:
+                raise ValueError("wrong tuple length.")
+        if newItem[2] < 0:
+            raise NotImplementedError("this method can't test for range ends when moving backwards yet.")
+        settings_list[i] = newItem
+    counters = [settings_list[i][0] for i in range(len(input_list))]
+    while True:
+        yield tuple(counters)
+        counters[0] += settings_list[0][2]
+        if counters[0] >= settings_list[0][1]: #change to incorporate signed diffs.
+            counters[0] = settings_list[0][0]
+            for rolloverIndex in range(1, len(counters)):
+                counters[rolloverIndex] += settings_list[rolloverIndex][2]
+                if counters[rolloverIndex] >= settings_list[rolloverIndex][1]:
+                    counters[rolloverIndex] = settings_list[rolloverIndex][0]
+                    continue
+                else:
+                    break # no more rollovers should happen.
+            else:
+                return
+
+assert [item for item in higher_range([(5,8), (1,4)])] == [(5,1), (6,1), (7,1), (5,2), (6,2), (7,2), (5,3), (6,3), (7,3)]
+
+
+    
+def get_at(data_to_access, labled_coords, access_order, bitcatted_axes="", dissolved_axes="", labled_axis_sizes=None):
+    if len(access_order) == 0:
+        # return now because there is probably no more accessing (narrowing) to be done. This silently allows coords with nonsense components, though.
+        return data_to_access
+    if not hasattr(data_to_access, "__len__"):
+        if access_order[0] in bitcatted_axes:
+            raise NotImplementedError("currently can't undo bit concatenation.")
+            exit(NOT_IMPLEMENTED_ERRLVL)
+            #when implemented, it will require knowledge of the axis' max size.
+            #also, bitcatting can only apply to axes that appear consecutively and immediately before the end of access_order.
+        else:
+            #return incomplete answer because narrowing can't continue. Maybe this should raise an exception.
+            return data_to_access
+    if len(data_to_access) == 0:
+        #return incomplete answer because the data is empty here.
+        return None
+        
+    if access_order[0] in labled_coords:
+        ac = labled_coords[access_order[0]]
+        return get_at(data_to_access[ac], labled_coords, access_order[1:])
+    else:
+        #this is where a change from an input access order to a different output access order might be made. Any coordinate components not specified in labled_coords could be swapped to change the orientation of the data. This would involve making the recursive calls to get_at include a new element in labled_coords. let this added element be the index used in iteration.
+        if len(access_order) > 1 and access_order[1] in dissolved_axes:
+            assert access_order[1] in labled_axis_sizes or access_order[0] in labled_axis_sizes
+            raise NotImplementedError("currently can't handle dissolved axes.")
+            exit(NOT_IMPLEMENTED_ERRLVL)
+            #this is where formats like [[r g b r g b r g b ...]...] will be handled.
+        else:
+            return [get_at(data_to_access[i], labled_coords, access_order[1:], bitcatted_axes=bitcatted_axes, dissolved_axes=dissolved_axes, labled_axis_sizes=labled_axis_sizes) for i in range(len(data_to_access))]
+
+assert get_at([[1,2,3],[4,5,6],[7,8,9]], {"x":2, "y":1}, "yx") == 6
+assert get_at([[1,2,3],[4,5,6],[7,8,9]], {"x":2}, "yx") == [3,6,9]
+
+assert get_at(([[1,2,3],[4,5,6],[7,8,9]], [[10,20,30],[40,50,60],[70,80,90]]), {"x":0, "y":2, "c":1}, "cyx") == 70
+assert get_at(([[1,2,3],[4,5,6],[7,8,9]], [[10,20,30],[40,50,60],[70,80,90]]), {"x":0, "y":2}, "cyx") == [7,70]
+
+
+
+
+
+
+def prepare_color(color_input, channel_depths):
+    if type(color_input) in {tuple, list}:
+        for componentIndex, component in enumerate(color_input):
+            assert component >= 0
+            assert component < 2**channel_depths[componentIndex]
+        if len(color_input) == len(channel_depths):
+            return color_input
+        else:
+            return (color_input + type(color_input)([0]*3))[:3]
+    elif isinstance(color_input, int):
+        assert 0 <= color_input
+        assert color_input < 2**channel_depths[0]
+        return (color_input, 0, 0)
+    else:
+        raise TypeError(str(type(color_input)))
+        
+    #elif isinstance(item, float):
+    #    assert 0 <= item <= 1
+    #    intVal = max(0, min(255, math.floor(item*256)))
+
+
+
+
+"""
+def run_basic():
+    size = (len(data), len(data[0]))
+    surface = pygame.Surface(size)
+
+    for y, row in enumerate(data):
+        for x, item in enumerate(row):
+            pixelColor = prepare_color(item)
+            surface.set_at((x,y), pixelColor)
+    pygame.image.save(surface, filename)
+"""
+
+def channel_count_to_pypng_color_mode(count):
+    assert count > 0
+    return ["l", "la", "rgb", "rgba"][count-1]
+
+def encode_pypng_row(color_seq):
+    def verifiedColor(color):
+        if isinstance(color, tuple):
+            assert len(color) == 3
+            return color
+        if isinstance(color, int):
+            return (color, color, color)
+        raise TypeError(color)
+    return bytes([(item if item is not None else 0) for color in color_seq for item in verifiedColor(color)])
+    
+
+class Canvas:
+    def __init__(self, size, library_name):
+        self.library_name = library_name
+        self.size = size
+        if self.library_name == "png":
+            self.data = [[None for x in range(size[0])] for y in range(size[1])]
+            def write_pixel(pixel_coord, pixel_color):
+                if len(pixel_color) != size[2]:
+                    # raise ValueError("tuples of size other than 3 are not yet supported when using pypng (aka \"png\")")
+                    raise ValueError("bad tuple length: {} (should be {}).".format(len(pixel_color), self.size[2]))
+                # self.data[pixel_coord[1]][pixel_coord[0]*self.size[2]:(pixel_coord[0]+1)*self.size[2]] = pixel_color
+                self.data[pixel_coord[1]][pixel_coord[0]] = pixel_color
+            def save_as(filename):
+                assert size[2] > 0
+                apparentColorMode = channel_count_to_pypng_color_mode(size[2])
+                # assert len(shape(self.data)) == 2
+                # image = png.from_array([bytes(row) for row in self.data], apparentColorMode)
+                # encodeRow = lambda inputList: 
+                image = png.from_array((encode_pypng_row(row) for row in self.data), apparentColorMode.upper())
+                try:
+                    image.save(filename)
+                except png.ProtocolError as pe:
+                    print("size={}, apparentColorMode={}.".format(size, apparentColorMode))
+                    if product(size) < 25:
+                        print(self.data)
+                    raise pe
+        elif self.library_name == "pygame":
+            self.data = pygame.Surface(self.size[:2])
+            def write_pixel(pixel_coord, pixel_color):
+                self.data.set_at(pixel_coord, pixel_color)
+            def save_as(filename):
+                pygame.image.save(self.data, filename)
+        else:
+            raise ValueError("unknown library: {}".format(library_name))
+        self.write_pixel = write_pixel
+        self.save_as = save_as
+        
+        
+
+    
+def run_nonstreaming(data, filename, library_name):
+    labledDataShape = labled_shape(data, keyword_args["access-order"])
+    #assert "c" in labledDataShape
+    if library_name == "png":
+        channel_depths = (8,)*(labledDataShape["c"] if "c" in labledDataShape else 3)
+        #assert len(channel_depths) != 3
+    else:
+        assert library_name == "pygame"
+        channel_depths = (8, 8, 8)
+    canvas = Canvas((labledDataShape["x"], labledDataShape["y"], len(channel_depths)), library_name)
+    apparentColorMode = channel_count_to_pypng_color_mode(len(channel_depths))
+    for y in range(labledDataShape["y"]):
+        for x in range(labledDataShape["x"]):
+            pixelColorData = get_at(data, {"x":x, "y":y}, keyword_args["access-order"])
+            pixelColor = prepare_color(pixelColorData, channel_depths)
+            if keyword_args["swizzle"] is not None:
+                pixelColor = tuple(pixelColor[apparentColorMode.index(channelKey)] for channelKey in keyword_args["swizzle"])
+            canvas.write_pixel((x,y), pixelColor)
+    canvas.save_as(filename)
+    
+# def run_png(filename, data):
+#     labledDataShape = labled_shape(data, keyword_args["access-order"])
+    
+def gen_stdin_lines():
+    #i = 0
+    for i in itertools.count():
+        print("wait. ", end="")
+        nextLine = sys.stdin.readline()
+        """
+        nextLineChars = []
+        while True:
+            newChar = sys.stdin.read(1)
+            #print(newChar,end="")
+            if newChar == "\n":
+                break
+            nextLineChars.append(newChar)
+        nextLine = "".join(nextLineChars)
+        """
+        print("line {} starts: {}.".format(i, nextLine[:128]))
+        if len(nextLine) == 0:
+            print("line length zero! ending!")
+            return
+        if nextLine == "STOP":
+            print("line says STOP! ending!")
+            return
+        if nextLine.startswith("STOP"):
+            print("line starts with STOP! ending!")
+            return
+        #if i == count:
+        #    print("line will not be supplied")
+        yield nextLine
+        #i += 1
+
+        
+class PeekableGenerator:
+    #this shouldn't exist. it should be replaced with itertools.tee.
+    def __init__(self, source_gen):
+        self.source_gen = source_gen
+        self.fridge = collections.deque([])
+    def peek(self):
+        result = next(self.source_gen)
+        self.fridge.append(result)
+        return result
+    def __next__(self):
+        if len(self.fridge) != 0:
+            result = self.fridge.popleft()
+            return result
+        else:
+            return next(self.source_gen)
+    def __iter__(self):
+        return self
+        
+def measure_time(input_fun):
+    def alt_fun(*args, **kwargs):
+        startTime=time.time()
+        input_fun(*args, **kwargs)
+        endTime=time.time()
+        totalTime = endTime-startTime
+        print("took {} seconds ({} minutes).".format(totalTime, totalTime/60))
+    return alt_fun
+    
+def pypng_streaming_save_square(filename, row_seq, height):
+    row_seq = gen_make_inexhaustible(row_seq) # prevent pypng from ever running out of lines.
+    row_seq = gen_take_only(row_seq, height) # fix issue where pypng complains about having more rows than it needs and crashes. Kinda weird but I still love you, pypng.
+    image = png.from_array(row_seq, "RGB", info={"height":height})
+    image.save(filename + "_" + str(time.time()) + ".png")
+    
+def pypng_streaming_save_squares(filename, row_seq, height):
+    peekableRowSeq = PeekableGenerator(row_seq)
+    for i in itertools.count():
+        try:
+            peekableRowSeq.peek() #may raise StopIteration.
+            pypng_streaming_save_square(filename+"_{}px{}inseq".format(height, i), peekableRowSeq, height)
+        except StopIteration:
+            return
+    assert False
+    
+    
+@measure_time
+def run_streaming(filename):
+    simpleLineSource = gen_stdin_lines()
+    notelessLineSource = (item for item in simpleLineSource if "#" not in item)
+    
+    peekableLineSource = PeekableGenerator(notelessLineSource)
+    borrowedLine = peekableLineSource.peek()
+    assumedHeight = len(eval(borrowedLine))
+    peekableLineSource.fridge.clear()
+    peekableLineSource.fridge.append(borrowedLine)
+    print("length {}.".format(assumedHeight))
+    
+    #iterableSource = (encodePyPngRow(eval(line)) for source in [[stolenLine], lineSource] for line in source)
+    iterableSource = (encode_pypng_row(eval(line)) for line in peekableLineSource)
+    
+    pypng_streaming_save_squares(filename, iterableSource, assumedHeight)
+    
+    
+    
+    
+def get_after_match(text_to_match, arg_to_test):
+    if arg_to_test.startswith(text_to_match):
+        return arg_to_test[len(text_to_match):]
+        
+def get_after_keyword_match(name_to_match, arg_to_test):
+    return get_after_match("--" + name_to_match + "=", arg_to_test)
+
+
+
+prog_args = sys.argv[1:]
+
+keyword_arg_descriptions = {
+    "access-order": "yx[c] in whatever order they must be applied to access the smallest data item in the input data.",
+    "swizzle": "[r][g][b][l][a], where each string position affects a corresponding output channel, and the letter at that position defines which input channel should be written to the output channel."
+}  
+keyword_args = {"access-order": "yxc", "swizzle": None}
+# in the future, it will be possible to use a similar looking definition to specify flatter data.
+# e.g.: 
+#   "yxc" -> [[[y0x0c0, y0x0c1], [y0x1c0, y0x1c1]], [[y1x0c0...]...]].
+#   "y(xc)" -> [[y0x0c0, y0x0c1, y0x1c0, y0x1c1], [y1x0c0...]].
+#   "(yx)c" -> [[y0x0c0, y0x0c1], [y0x1c0, y0x1c1], [y1x0c0...]...]].
+#   "(yxc)" -> [y0x0c0, y0x0c1, y0x1c0, y0x1c1, y1x0c0...].
+#   keep in mind that no new operations are needed to make rows come in unbound groups of three for color channels - this is the same as "(yc)x".
+
+nonoption_args = []
+
+USAGE_STRING = "Usage: [OPTION] <FILE> <DATA>"
+HELP_STRING = """
+Create FILE png described by DATA.
+if DATA is '-', all data will be read from stdin. Currently this requires that each line contains one row of pixel info. Lines starting with '#' will be printed, and not parsed.
+
+optional arguments:
+--help displays this message.
+there are some others but they aren't documented yet."""
+
+    
+
+if len(sys.argv[0]) > 0: # if being run as a command:
+    if len(prog_args) == 0:
+        print(USAGE_STRING)
+        exit(1)
+    for arg in prog_args:
+        argSuccess = False
+        if arg.startswith("--"):
+            if arg == "--help":
+                print(HELP_STRING)
+                exit(0)
+            for keyword_arg_name in keyword_args.keys():
+                newValue = get_after_keyword_match(keyword_arg_name, arg)
+                if newValue is None:
+                    continue
+                keyword_args[keyword_arg_name] = newValue
+                assert keyword_args[keyword_arg_name] is not None, keyword_arg_name
+
+                argSuccess = True
+            if argSuccess == False:
+                print("unknown option: {}".format(arg))
+                exit(2)
+        else:
+            nonoption_args.append(arg)
+    #for testValue in keyword_args.values():
+    #    assert testValue is not None, keyword_args
+
+    assert len(nonoption_args) == 2
+
+    prog_save_filename = nonoption_args[0]
+    assert len(prog_save_filename) > 0
+    assert ".png" in prog_save_filename
+
+    if nonoption_args[1] == "-":
+        run_streaming(prog_save_filename)
+        print("run_streaming is over.")
+    else:
+        prog_data = eval(nonoption_args[1])
+        assert len(shape(prog_data)) in {1, 2, 3}
+
+        run_nonstreaming(prog_data, prog_save_filename, "png")
+        print("run_nonstreaming is over.")
+
+    print("exiting python.")
+    exit(0)
